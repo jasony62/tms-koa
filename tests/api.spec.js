35f314e119b69769f9c0cd662b606026a58a2c3d
jasony62 <jasony62@qq.com>
2019-09-13 23:42:24 +0800

    update code

--- tests/api.spec.js
+++
@@ -1,9 +0,0 @@
-describe("#api", () => {
-  describe("#app.js", () => {
-    test("route()", () => {
-      const CtrlClass = require("../../apis/main")
-      let ctrl = new CtrlClass()
-      expect(ctrl.version()).toMatchObject({ code: 0, result: "0.1" })
-    })
-  })
-})
