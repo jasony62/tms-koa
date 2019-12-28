929eb13d2780ca331b7a0ada781d759709ba9211
jasony62 <jasony62@qq.com>
2019-12-28 17:09:45 +0800

    支持采用jwt方式进行用户身份验证

--- config/app.sample.js
+++ config/app.sample.js
@@ -1,12 +1,16 @@
 module.exports = {
-    port: 3001,
-    name: 'tms-koa-0',
-    router: {
-        auth: {
-            prefix: ''
-        },
-        controllers: {
-            prefix: ''
-        }
+  port: 3001,
+  name: 'tms-koa-0',
+  router: {
+    auth: {
+      prefix: ''
+    },
+    controllers: {
+      prefix: ''
     }
+  },
+  jwt: {
+    privateKey: 'tms-koa-secret',
+    expiresIn: 3600
+  }
 }
