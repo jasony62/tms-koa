5737725284fb9f4c245ef8f13fca747d44f46ff2
jasony62 <jasony62@qq.com>
2020-01-02 21:17:10 +0800

    提供内置用户认证方法

--- auth/captcha.js
+++ auth/captcha.js
@@ -1,11 +1,11 @@
-const { Captcha } = require("../lib/app")
+const { Captcha } = require('../lib/app')
 /**
  * 获取换取access_token的验证信息
  */
 module.exports = function(ctx) {
-    let veri = new Captcha()
+  let veri = new Captcha()
 
-    veri.code = '4321'
+  veri.code = '4321'
 
-    return veri
+  return [true, '4321']
 }
