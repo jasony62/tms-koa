3a694ca6f7243e8d685e00ec7bba5d6be6171703
jasony62 <jasony62@qq.com>
2020-01-03 16:53:27 +0800

    优化jwt认证机制

--- config/app.sample.js
+++ config/app.sample.js
@@ -9,8 +9,18 @@ module.exports = {
       prefix: ''
     }
   },
-  jwt: {
-    privateKey: 'tms-koa-secret',
-    expiresIn: 3600
+  auth: {
+    captcha: {
+      //path: '/auth/captcha.js', // 指定验证码实现方法
+      code: 'a1z9' // 预制验证码（没有指定外部实现方法时生效）
+    },
+    client: {
+      //path: '/auth/client.js', // 指定用户认证实现方法
+      accounts: [{ id: 1, username: 'user1', password: '123456' }] // 预制用户账号（没有指定外部实现方法时生效）
+    },
+    jwt: {
+      privateKey: 'tms-koa-secret',
+      expiresIn: 3600
+    }
   }
 }
