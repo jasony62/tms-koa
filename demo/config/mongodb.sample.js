01dc6ee3c4d389c01f9a0006862c8d97700724e0
liao1a <153887111@qq.com>
2020-06-18 14:13:35 +0800

    连接mongodb支持用户名和密码

--- demo/config/mongodb.sample.js
+++ demo/config/mongodb.sample.js
@@ -1,6 +1,8 @@
 module.exports = {
   disabled: false, // 可省略
   master: {
+    // user: "root",
+    // password: "root",
     host: 'localhost',
     port: 27017
   }
--- demo/config/mongoose.sample.js
+++ demo/config/mongoose.sample.js
@@ -1,5 +1,7 @@
 module.exports = {
   disabled: false, // 可省略
+  // user: "root",
+  // password: "root",
   host: 'localhost',
   port: 27017,
   database: 'test'
